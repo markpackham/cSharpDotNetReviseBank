01d7e4abc82ec67a1d1934d61625e2db724fee97
mark <markpackham1@gmail.com>
2022-01-20 21:22:17 +0000

    C# .Net revise https://www.youtube.com/watch?v=xLhm3bEG__c&list=PLdo4fOcmZ0oVxKLQCHpiUWun7vlJJvUiN&index=17

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MySuperBank;
+
+var account = new BankAccount("01","Bob",10000);
+Console.WriteLine($"Account {account.Number} was created for {account.Owner} with £{account.Balance}.");
