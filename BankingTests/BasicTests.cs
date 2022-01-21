885391d65bdec6d518435ba51c4223e3b7f28e90
mark <markpackham1@gmail.com>
2022-01-21 14:01:52 +0000

    check error thrown for using money value out of range
    https://www.youtube.com/watch?v=PFtxjTMl5N0

--- BankingTests/BasicTests.cs
+++ BankingTests/BasicTests.cs
@@ -7,18 +7,30 @@ namespace BankingTests
     public class BasicTests
     {
         [Fact]
-        public void Test1()
+        public void TrueIsTrue()
         {
              Assert.True(true);
         }
 
         [Fact]
-        public void Test2()
+        public void RejectGoingOverOverdraft()
         {
             var account = new BankAccount("Bob", 10000);
 
             Assert.Throws<InvalidOperationException>(
+                // () => reminds me of JavaScript
                 () => account.MakeWithdrawal(999999, DateTime.Now, "Attempt to go over overdraf")
+                );
+        }
+
+        [Fact]
+        public void RejectNativeMoney()
+        {
+            var account = new BankAccount("Bob", 10000);
+
+            Assert.Throws<ArgumentOutOfRangeException>(
+                // () => reminds me of JavaScript
+                () =>  new BankAccount("Attempt to go use negative money",-30)
                 );
         }
     }
