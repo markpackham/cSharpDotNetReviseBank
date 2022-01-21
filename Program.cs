00f9ebb2694f0c84d004821105bcaab05f307993
mark <markpackham1@gmail.com>
2022-01-21 11:21:41 +0000

    add dependancy to BankyStuffLibrary
    https://youtu.be/Jms3C-J-m2M?t=491

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using MySuperBank;
+
+using BankyStuffLibrary;
 
 var account = new BankAccount("Bob",10000);
 Console.WriteLine($"Account {account.Number} was created for {account.Owner} with £{account.Balance}.");
