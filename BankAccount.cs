41b328aa8a6d3f89040b1876616fe354d37d8391
mark <markpackham1@gmail.com>
2022-01-21 08:42:34 +0000

    add transactions list

--- BankAccount.cs
+++ BankAccount.cs
@@ -1,17 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MySuperBank
+﻿namespace MySuperBank
 {
     public class BankAccount
     {
         public string Number { get; }
         public string Owner { get; set; }
-        public decimal Balance { get; }
+        public decimal Balance
+        {
+
+            get
+            {
+                decimal balance = 0;
+                foreach(var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+            };
+        }
         private static int accountNumberSeed = 1234567890;
+        private List<Transaction> allTransactions = new List<Transaction>();
 
         public BankAccount(string owner, decimal balance)
         {
